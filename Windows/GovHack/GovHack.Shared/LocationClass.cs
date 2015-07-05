48448dacfef74eab4270a24ac7f0c8081211c95a
AaronYeoh <ayeo722@aucklanduni.ac.nz>
2015-07-05 16:18:23 +1200

    Commits images

---
+++ Windows/GovHack/GovHack.Shared/LocationClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GovHack
+{
+    public class LocationClass
+    {
+        public List<Locations> Locations;
+    }
+
+    public class Locations
+    {
+        public string Produce { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Description { get; set; }
+        public double AveragePricePerKilo { get; set; }
+    }
+}
---
+++ Windows/GovHack/GovHack.Shared/LocationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Newtonsoft.Json;
+
+namespace GovHack
+{
+    public class LocationsController
+    {
+        public List<Locations> locationsList;
+        private bool alreadyRead = false;
+        public async Task<List<Locations>> GetLocations()
+        {
+            if (alreadyRead)
+            {
+                return locationsList;
+            }
+
+            Uri uri = new Uri("ms-appx:///Assets/locations.json");
+            StorageFile file = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(uri);
+
+            LocationClass locations;
+            using (StreamReader r = new StreamReader(await file.OpenStreamForReadAsync()))
+            {
+                string json = r.ReadToEnd();
+                locations = JsonConvert.DeserializeObject<LocationClass>(json);
+                locationsList = locations.Locations;
+            }
+            alreadyRead = true;
+            return locationsList;
+        }
+
+
+
+
+    
+    }
+}
