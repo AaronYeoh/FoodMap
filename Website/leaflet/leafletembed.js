28262338a5b4a2d188d97b7f30aefdcc2fdbe220
AaronYeoh <AaronYeoh@users.noreply.github.com>
2015-08-07 16:00:47 +1200

    Update leafletembed.js

--- Website/leaflet/leafletembed.js
+++ Website/leaflet/leafletembed.js
@@ -16,21 +16,22 @@ function initmap() {
 	map.setView(new L.LatLng(-36.840556,174.74),9);
 	map.addLayer(osm);
 	
+
 	//Let's add a marker in Auckland
 	var marker = L.marker([-36.840556,174.74]).addTo(map);
 	
 	//How about another marker?
 	//shadowUrl: '',
 	var lemonIcon = L.icon({
-    	iconUrl: '../resources/lemon.png',
+    iconUrl: "http://www.tuxpaint.org/stamps/stamps-thumbs/stamps/food/fruit/cartoon/lemon.jpg",
     
 
-	    iconSize:     [38, 95], // size of the icon
-	    shadowSize:   [50, 64], // size of the shadow
-	    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-	    shadowAnchor: [4, 62],  // the same for the shadow
-	    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-	});
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 	
 	
 	var marker = L.marker([-36.840560,174.74], {icon: lemonIcon}).addTo(map);
