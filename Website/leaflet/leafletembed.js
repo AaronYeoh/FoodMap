daecfada162ae6bce3a19a5b01669bf46d0a0aba
AaronYeoh <AaronYeoh@users.noreply.github.com>
2015-08-07 16:02:43 +1200

    Update leafletembed.js

--- Website/leaflet/leafletembed.js
+++ Website/leaflet/leafletembed.js
@@ -26,10 +26,10 @@ function initmap() {
     iconUrl: "http://www.tuxpaint.org/stamps/stamps-thumbs/stamps/food/fruit/cartoon/lemon.jpg",
     
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
+    iconSize:     [38, 38], // size of the icon
+    //shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
+    //shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 	
